34c3750cc642e845315e9b0fbbebd79af5f4a090
Ro-B-Fernandez <104107565+Ro-B-Fernandez@users.noreply.github.com>
2022-05-09 19:22:21 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+//Eureka
+
+let contador : number = 1;
+let claveIngresada : string;
+
+while (contador <= 3 && claveIngresada != "eureka") {
+
+claveIngresada = prompt("Introduzca la clave");
+contador ++;
+}
+if (claveIngresada == "eureka") {
+console.log("Clave correcta" );
+} else {
+console.log("Ya no tiene más intentos" );
+} 
